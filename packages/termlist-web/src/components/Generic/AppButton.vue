05695999a05d3f7b85ef48a84a401017510d3d27
Patrick Sletvold <patricksletvold@hotmail.com>
2017-11-09 20:37:39 +0100

    Use AppButton component in ModalRemove too

--- packages/termlist-web/src/components/Generic/AppButton.vue
+++ packages/termlist-web/src/components/Generic/AppButton.vue
@@ -1,9 +1,9 @@
 <template>
-<button class="button" :class="{'is-primary':primary}" @click="click"><slot>OK</slot></button>
+<button class="button" :class="{'is-primary':primary,'is-danger':danger}" @click="click"><slot>OK</slot></button>
 </template>
 <script>
 export default {
-  props: ['primary'],
+  props: ['primary', 'danger'],
   methods: {
     click(e) {
       this.$emit('click', e);
