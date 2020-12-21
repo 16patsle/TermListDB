94dc8f5be63c353d1caa5c15f349a9afafb390fb
Patrick Sletvold <patricksletvold@hotmail.com>
2020-12-21 19:32:22 +0100

    Add doc comments for SearchType

--- packages/termlist-web/src/types/SearchType.ts
+++ packages/termlist-web/src/types/SearchType.ts
@@ -1,8 +1,20 @@
 import type { FieldType } from './FieldType'
 
 export type SearchType = {
+  /**
+   * String to search for
+   */
   search: string
+  /**
+   * Selected field to search in, or 'all'
+   */
   selected: string
+  /**
+   * All the fields
+   */
   fields: FieldType[]
+  /**
+   * The field to sort by.
+   */
   field?: string
 }
