82621ac07957ac0050b77d44aec33964e5288669
Patrick Sletvold <patricksletvold@hotmail.com>
2021-08-22 17:32:21 +0200

    Convert AppSelect to use v-model

--- packages/termlist-web/src/components/Generic/AppSelect.vue
+++ packages/termlist-web/src/components/Generic/AppSelect.vue
@@ -1,47 +1,38 @@
 <template>
   <div :class="{ 'is-fullwidth': fullwidth }" class="select">
-    <select @change="change">
-      <option v-if="defaultOption" value disabled :selected="value === ''">{{ defaultOptionName }}</option>
+    <select v-model="value">
+      <option v-if="defaultOption" value="" disabled>{{ defaultOptionName }}</option>
       <option
         v-for="option in options"
         :key="option.name"
         :value="option.name"
-        :selected="value === option.name"
       >{{ option.ui }}</option>
     </select>
   </div>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
+import { computed } from "vue";
 import type { SelectOptionType } from '../../types/SelectOptionType'
 
-withDefaults(defineProps<{
+const props = withDefaults(defineProps<{
   defaultOption?: boolean
   defaultOptionName?: string
   options: SelectOptionType[],
   fullwidth?: boolean
-  value?: string
+  modelValue?: string
 }>(), {
   defaultOption: true,
   defaultOptionName: '',
   fullwidth: false,
-  value: ''
+  modelValue: ''
 })
 
 const emit = defineEmits<{
-  (e: 'input', value: string): void
-  (e: 'change', value: string, event: HTMLInputElement): void
+  (e: 'update:modelValue', value: string): void
 }>()
 
-const value = ref('')
-
-const setValue = (value: string): void => {
-  emit('input', value)
-}
-
-const change = (e: Event): void => {
-  const target = (e.target as HTMLInputElement)
-  setValue(target.value)
-  emit('change', value.value, target)
-}
+const value = computed({
+  get: () => props.modelValue,
+  set: val => emit('update:modelValue', val)
+})
 </script>
