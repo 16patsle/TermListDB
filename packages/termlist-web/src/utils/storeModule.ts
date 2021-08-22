839045c315e94295a4ab45cafc729c8a1072ac66
Patrick Sletvold <patricksletvold@hotmail.com>
2021-08-22 11:33:38 +0200

    Remove vuex-module-decorators

--- packages/termlist-web/src/utils/storeModule.ts
+++
@@ -1,6 +0,0 @@
-import { getModule } from 'vuex-module-decorators'
-import store from '../store'
-import StoreModule from '../storeModule'
-
-const storeModule = getModule(StoreModule, store)
-export default storeModule
