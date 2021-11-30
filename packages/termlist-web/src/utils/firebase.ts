99a407dae9c95ffad48837312052f10716e44e41
Patrick Sletvold <patricksletvold@hotmail.com>
2021-11-30 11:18:16 +0100

    Remove firebase compat code

--- packages/termlist-web/src/utils/firebase.ts
+++ packages/termlist-web/src/utils/firebase.ts
@@ -1,9 +1,9 @@
-import firebase from 'firebase/compat/app'
+import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import TermDatabase from '../database'
 
 /* global process */
-export const firebaseApp = firebase.initializeApp({
+export const firebaseApp = initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
