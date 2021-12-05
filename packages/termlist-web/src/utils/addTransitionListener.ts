44b1df047cd2c259fd2cda46726f7136b1f3855c
Patrick Sletvold <patricksletvold@hotmail.com>
2021-12-05 20:13:16 +0100

    Add proper types for addTransitionListener

--- packages/termlist-web/src/utils/addTransitionListener.ts
+++ packages/termlist-web/src/utils/addTransitionListener.ts
@@ -1,7 +1,11 @@
 import { onMounted, onUnmounted } from 'vue'
+import { StateListener } from 'xstate/lib/interpreter'
+import { GlobalContext, GlobalEvent } from '../machines/globalMachine'
 import { globalService } from '../machines/globalService'
 
-export function addTransitionListener(listener: (state: any) => void) {
+export function addTransitionListener(
+  listener: StateListener<GlobalContext, GlobalEvent>
+) {
   onMounted(() => globalService.onTransition(listener))
   onUnmounted(() => globalService.off(listener))
 }
