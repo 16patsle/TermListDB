b12bbb76a8e02a5e479365db4b00b24e79a366d2
Patrick Sletvold <patricksletvold@hotmail.com>
2021-12-08 21:17:23 +0100

    Add stories for AppSelect and AppModal too

---
+++ packages/termlist-web/src/components/Generic/AppModal.stories.ts
@@ -0,0 +1,85 @@
+import AppModal from './AppModal.vue'
+import AppButton from './AppButton.vue'
+
+export default {
+  title: 'Generic/AppModal',
+  component: AppModal,
+  argTypes: {
+    onOkClick: { action: 'okClicked' },
+    onClose: { action: 'closed' },
+  },
+}
+
+const Template = args => ({
+  components: { AppModal },
+  setup() {
+    return { args }
+  },
+  template: `
+    <AppModal v-bind="args">
+      <template #modal-body>
+        Modal Body
+      </template>
+    </AppModal>
+  `,
+})
+
+export const Basic = Template.bind({})
+Basic.args = {
+  title: 'Modal Title',
+  isActive: true,
+}
+
+export const NoCloseAllowed = Template.bind({})
+NoCloseAllowed.args = {
+  ...Basic.args,
+  closeAllowed: false,
+}
+
+export const CustomText = Template.bind({})
+CustomText.args = {
+  ...Basic.args,
+  title: 'Custom Title',
+  okText: 'Custom OK Text',
+  cancelText: 'Custom Cancel Text',
+}
+
+export const CustomFooter = args => ({
+  components: { AppModal, AppButton },
+  setup() {
+    return { args }
+  },
+  template: `
+    <AppModal v-bind="args">
+      <template #modal-body>
+        Modal Body
+      </template>
+      <template #modal-footer>
+        <AppButton danger>Danger Button</AppButton>
+      </template>
+    </AppModal>
+  `,
+})
+CustomFooter.args = {
+  ...Basic.args,
+}
+
+export const CustomHeader = args => ({
+  components: { AppModal, AppButton },
+  setup() {
+    return { args }
+  },
+  template: `
+    <AppModal v-bind="args">
+      <template #title>
+        <AppButton danger>Header Button</AppButton>
+      </template>
+      <template #modal-body>
+        Modal Body
+      </template>
+    </AppModal>
+  `,
+})
+CustomHeader.args = {
+  ...Basic.args,
+}
---
+++ packages/termlist-web/src/components/Generic/AppSelect.stories.ts
@@ -0,0 +1,55 @@
+import AppSelect from './AppSelect.vue'
+
+export default {
+  title: 'Generic/AppSelect',
+  component: AppSelect,
+  argTypes: {
+    'onUpdate:modelValue': { action: 'update:modelValue' },
+  },
+}
+
+const Template = args => ({
+  components: { AppSelect },
+  setup() {
+    return { args }
+  },
+  template: '<AppSelect v-bind="args"></AppSelect>',
+})
+
+export const Basic = Template.bind({})
+Basic.args = {
+  options: [
+    {
+      name: 'option1',
+      ui: 'Option 1',
+    },
+    {
+      name: 'option2',
+      ui: 'Option 2',
+    },
+  ],
+}
+
+export const NoDefault = Template.bind({})
+NoDefault.args = {
+  ...Basic.args,
+  defaultOption: false,
+}
+
+export const NoDefaultWithValue = Template.bind({})
+NoDefaultWithValue.args = {
+  ...NoDefault.args,
+  modelValue: 'option2',
+}
+
+export const CustomDefault = Template.bind({})
+CustomDefault.args = {
+  ...Basic.args,
+  defaultOptionName: 'Custom Default',
+}
+
+export const Fullwidth = Template.bind({})
+Fullwidth.args = {
+  ...Basic.args,
+  fullwidth: true,
+}
