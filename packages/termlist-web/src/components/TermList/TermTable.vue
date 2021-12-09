5cdd588545625c0681c5c3458251e085381c5f92
Patrick Sletvold <patricksletvold@hotmail.com>
2021-12-09 14:36:18 +0100

    Add TermTable component

---
+++ packages/termlist-web/src/components/TermList/TermTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <AppTable>
+    <template #table-header>
+      <TermHeader />
+    </template>
+    <template #table-body>
+      <TermRow
+        v-for="term of terms"
+        :key="term._id"
+        :term="term"
+        @edit="$emit('edit', $event)"
+        @remove="$emit('remove', $event)"
+      />
+    </template>
+  </AppTable>
+</template>
+
+<script lang="ts" setup>
+import AppTable from '../Generic/AppTable.vue'
+import TermHeader from './TermHeader.vue'
+import TermRow from './TermRow.vue'
+
+import type { TermType } from '../../types/TermType'
+
+defineProps<{ terms: TermType[] }>()
+
+defineEmits<{
+  (e: 'edit', term: TermType): void
+  (e: 'remove', term: TermType): void
+}>()
+</script>
