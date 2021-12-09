3932161f618901a559746e1ad49ff976368189d4
Patrick Sletvold <patricksletvold@hotmail.com>
2021-12-09 10:44:37 +0100

    Add stories for TermRow

---
+++ packages/termlist-web/src/components/TermList/TermRow.stories.ts
@@ -0,0 +1,35 @@
+import type { Story } from '@storybook/vue3'
+import TermRow from './TermRow.vue'
+
+export default {
+  title: 'TermList/TermRow',
+  component: TermRow,
+  argTypes: {
+    onEdit: { action: 'edited' },
+    onRemove: { action: 'removed' },
+  },
+}
+
+const Template: Story = args => ({
+  components: { TermRow },
+  setup() {
+    return { args }
+  },
+  template: '<TermRow v-bind="args"></TermRow>',
+})
+
+export const Basic = Template.bind({})
+Basic.args = {
+  term: {
+    _id: 'fake id',
+    date: '2017-08-14T14:07:48.197Z',
+    desc: 'Fake description',
+    term: 'Fake term',
+    type: 'verb',
+  },
+}
+
+export const Empty = Template.bind({})
+Empty.args = {
+  term: {},
+}
