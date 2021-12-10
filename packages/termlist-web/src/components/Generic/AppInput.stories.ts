5bd19c5d8f2c0a7d0cfda930a54e4dcea587284a
Patrick Sletvold <patricksletvold@hotmail.com>
2021-12-10 09:25:40 +0100

    Add AppInput component and stories

---
+++ packages/termlist-web/src/components/Generic/AppInput.stories.ts
@@ -0,0 +1,77 @@
+import type { Story } from '@storybook/vue3'
+import AppInput from './AppInput.vue'
+
+export default {
+  title: 'Generic/AppInput',
+  component: AppInput,
+  argTypes: {
+    'onUpdate:modelValue': { action: 'update:modelValue' },
+    type: {
+      options: [
+        'text',
+        'email',
+        'password',
+        'number',
+        'tel',
+        'url',
+        'search',
+        'date',
+        'time',
+        'datetime',
+        'datetime-local',
+        'month',
+        'week',
+      ],
+      control: 'select',
+    },
+  },
+}
+
+const Template: Story = args => ({
+  components: { AppInput },
+  setup() {
+    return { args }
+  },
+  template: '<AppInput v-bind="args" />',
+})
+
+export const Basic = Template.bind({})
+Basic.args = {
+  placeholder: 'Basic input',
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  primary: true,
+  placeholder: 'Danger input',
+}
+
+export const Success = Template.bind({})
+Success.args = {
+  success: true,
+  placeholder: 'Danger input',
+}
+
+export const Warning = Template.bind({})
+Warning.args = {
+  warning: true,
+  placeholder: 'Danger input',
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  danger: true,
+  placeholder: 'Danger input',
+}
+
+export const Password = Template.bind({})
+Password.args = {
+  type: 'password',
+  placeholder: 'Password input',
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  disabled: true,
+  placeholder: 'Disabled input',
+}
---
+++ packages/termlist-web/src/components/Generic/AppInput.vue
@@ -0,0 +1,75 @@
+<template>
+  <input
+    v-model="value"
+    class="input"
+    :class="{
+      'is-primary': primary,
+      'is-success': success,
+      'is-warning': warning,
+      'is-danger': danger,
+      'is-loading': loading,
+    }"
+    :type="type"
+    :disabled="disabled"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    type?:
+      | 'text'
+      | 'email'
+      | 'password'
+      | 'number'
+      | 'tel'
+      | 'url'
+      | 'search'
+      | 'date'
+      | 'time'
+      | 'datetime'
+      | 'datetime-local'
+      | 'month'
+      | 'week'
+    placeholder?: string
+    primary?: boolean
+    success?: boolean
+    warning?: boolean
+    danger?: boolean
+    loading?: boolean
+    disabled?: boolean
+    modelValue?: string
+  }>(),
+  {
+    type: 'text',
+    placeholder: '',
+    primary: false,
+    success: false,
+    warning: false,
+    danger: false,
+    loading: false,
+    disabled: false,
+    modelValue: '',
+  }
+)
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+const value = computed({
+  get: () => props.modelValue,
+  set: val => emit('update:modelValue', val),
+})
+</script>
+
+<style lang="scss">
+@import 'bulma/sass/utilities/controls';
+@import 'bulma/sass/utilities/extends';
+@import 'bulma/sass/form/shared';
+@import 'bulma/sass/form/tools';
+@import 'bulma/sass/form/input-textarea';
+</style>
