c6ba8cdaccaf3325fd4828360656556cd5936379
Patrick Sletvold <patricksletvold@hotmail.com>
2023-03-15 20:28:40 +0100

    Migrate to vite

--- packages/termlist-web/src/utils/initializeFirebase.ts
+++ packages/termlist-web/src/utils/initializeFirebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 
 export const firebaseApp = initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 })
