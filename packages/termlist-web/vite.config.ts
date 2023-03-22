4a2bcc52276336623637cb96d550d772ec0f6e9c
Patrick Sletvold <patricksletvold@hotmail.com>
2023-03-22 18:30:30 +0100

    Generate webpack stats with vite/rollup for relative-ci

--- packages/termlist-web/vite.config.ts
+++ packages/termlist-web/vite.config.ts
@@ -3,11 +3,16 @@ import vue from '@vitejs/plugin-vue'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { webpackStats } from 'rollup-plugin-webpack-stats';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { target: browserslistToEsbuild() },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // Output webpack-stats.json file
+    webpackStats(),
+  ],
   resolve: {
     alias: {
       '@': path.join(path.dirname(fileURLToPath(import.meta.url)), 'src'),
